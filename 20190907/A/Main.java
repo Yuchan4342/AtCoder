0b240791ff4ba84b983bf21f675b8c65411608e6
Yuto Iwata <jyuchan415@gmail.com>
2019-09-07 15:22:13 +0900

    BufferedReader 生成部分を関数化.

--- 20190907/A/Main.java
+++ 20190907/A/Main.java
@@ -3,24 +3,25 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		try{
-			InputStreamReader isr =new InputStreamReader(System.in);
-	   		BufferedReader br =new BufferedReader(isr);
-	        String dateString = br.readLine();
-	       	System.out.println(isHeisei(dateString) ? "Heisei" : "TBD");
+		try {
+	   		BufferedReader br = createBufferedReader();
+	        String line = br.readLine();
+	        StringTokenizer st = new StringTokenizer(line, " ");
+	        int input = Integer.parseInt(st.nextToken());
+	       	System.out.println(method(input));
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
 	}
 
-	private static boolean isHeisei(final String dateString) {
-		StringTokenizer st = new StringTokenizer(dateString, "/");
-		final int y = Integer.parseInt(st.nextToken());
-	    final int m = Integer.parseInt(st.nextToken());
-	    final int d = Integer.parseInt(st.nextToken());
-	    if (m <= 4) {
-	    	return true;
-	    }
-		return false;
+	private static int method(final int a) {
+		return a;
+	}
+
+	// 以下問題問わずに共通部分
+	// 標準入力の BufferedReader を返す.
+	private static BufferedReader createBufferedReader() {
+		InputStreamReader isr =new InputStreamReader(System.in);
+	   	return new BufferedReader(isr);
 	}
 }
--- Template.java
+++ Template.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			InputStreamReader isr =new InputStreamReader(System.in);
-	   		BufferedReader br =new BufferedReader(isr);
+	   		BufferedReader br = createBufferedReader();
 	        String str = br.readLine();
+	        StringTokenizer st = new StringTokenizer(str, " ");
 	        int input = Integer.parseInt(str);
 	       	System.out.println(method(input));
 		} catch (IOException ioe) {
@@ -16,5 +16,12 @@ public class Main {
 
 	private static int method(final int a) {
 		return a;
+	}
+
+	// 以下問題問わずに共通部分
+	// 標準入力の BufferedReader を返す.
+	private static BufferedReader createBufferedReader() {
+		InputStreamReader isr =new InputStreamReader(System.in);
+	   	return new BufferedReader(isr);
 	}
 }

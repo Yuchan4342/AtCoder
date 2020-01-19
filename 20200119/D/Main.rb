ddc0df81b2177f6ab49cdbcbf5c07769e42a1200
Yuto Iwata <jyuchan415@gmail.com>
2020-01-19 22:58:03 +0900

    ABC152のD問題をAC

--- 20200119/D/Main.rb
+++ 20200119/D/Main.rb
@@ -1,70 +1,32 @@
-def handstand(n)
-	num_digits = n.to_s.length
+def c(n, i, j)
+	k = 1
+	digits = 1
 	count = 0
-	(1..num_digits).each do |digits_a|
-		(1..digits_a).each do |digits_b|
-			if digits_b == 1
-				if digits_a == 1
-					count += n >= 9 ? 9 : n
-				elsif digits_a < num_digits
-					count += 9 * (10 ** (digits_a - 2)) * 2
-				else
-					first_digit = n.to_s.split('')[0].to_i
-					count += (first_digit - 1) * (10 ** (digits_a - 2)) * 2
-					array = Array.new(digits_a) { '0' }
-					array[0] = first_digit.to_s
-					array[array.length - 1] = first_digit.to_s
-					a = array.join.to_i
-					while a <= n
-						array = a.to_s.split('')
-						count += 2 if array.first.to_i == first_digit and array.last.to_i == first_digit
-						a += 1
-					end
-				end
-			else
-				if digits_a < num_digits
-					count += 9 * 9 * (10 ** (digits_a - 2)) * (10 ** (digits_b - 2))
-				elsif digits_a > digits_b
-					first_digit = n.to_s.split('')[0].to_i
-					(1...first_digit).each do |i|
-						(1..9).each do |j|
-							count += (10 ** (digits_a - 2)) * (10 ** (digits_b - 2)) * 2
-						end
-					end
-					array = Array.new(digits_a) { '0' }
-					array[0] = first_digit.to_s
-					array[array.length - 1] = 1.to_s
-					a = array.join.to_i
-					while a <= n
-						array = a.to_s.split('')
-						if (array.first.to_i == first_digit and array.last.to_i == j) or (array.first.to_i == j and array.last.to_i == first_digit)
-							count += 2
-						end
-						a += 1
-					end
-				else
-					first_digit = n.to_s.split('')[0].to_i
-					(1..first_digit).each do |i|
-						(1..first_digit).each do |j|
-							if i < first_digit || j < first_digit
-								count += (10 ** (digits_a - 2)) * (10 ** (digits_b - 2))
-							elsif i == first_digit
-								array = Array.new(digits_a) { '0' }
-								array[0] = i.to_s
-								array[array.length - 1] = j.to_s
-								a = array.join.to_i
-								while a <= n
-									array = a.to_s.split('')
-									if (array.first.to_i == i and array.last.to_i == j) or (array.first.to_i == j and array.last.to_i == i)
-										count += 2
-									end
-									a += 1
-								end
-							end
-						end
-					end
-				end
+	while true
+		k = 10 ** (digits - 1) * i
+		while k / (10 ** (digits - 1)) == i and k <= n
+			count += 1 if k % 10 == j
+			k += 1
+		end
+		break if k > n
+
+		digits += 1
+	end
+	count
+end
+
+def handstand(n)
+	array = Array.new(9) {Array.new(9, -1)}
+	count = 0
+	(1..9).each do |i|
+		(1..9).each do |j|
+			if array[i - 1][j - 1] == -1
+				array[i - 1][j - 1] = c(n, i, j)
 			end
+			if array[j - 1][i - 1] == -1
+				array[j - 1][i - 1] = c(n, j, i)
+			end
+			count += array[i - 1][j - 1] * array[j - 1][i - 1]
 		end
 	end
 	count
